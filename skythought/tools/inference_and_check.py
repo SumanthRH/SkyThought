cc1383fe3538694a38180cb105d017103b5aa92f
SumanthRH <sumanthrh@anyscale.com>
2025-01-24 23:41:37 +0000

    x
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- skythought/tools/inference_and_check.py
+++ skythought/tools/inference_and_check.py
@@ -305,26 +305,8 @@ def perform_inference_and_save(handler: TaskHandler, temperatures, max_tokens, r
     with open(result_file, 'w', encoding='utf-8') as file:
         json.dump(results, file, ensure_ascii=False, indent=4, cls=NumpyEncoder)
 
-def main():
-    parser = argparse.ArgumentParser(description="Unified inference and checking for different datasets/tasks.")
-    parser.add_argument("--dataset", type=str, required=True, choices=list(TASK_HANDLERS.keys()), help="Dataset to process.")
-    parser.add_argument("--model", type=str, required=True, default="Qwen/QwQ-32B-Preview", help="The model to run.")
-    parser.add_argument("--tp", type=int, default=8, help="Tensor Parallelism Degree")
-    parser.add_argument("--max_tokens", type=int, default=32768, help="Max tokens for the model.")
-    parser.add_argument("--split", type=str, default="train", help="Split to use for apps (e.g., train, test).")
-    parser.add_argument("--source", type=str, help="Source for the dataset.")
-    parser.add_argument("--start", type=int, default=0, help="Start index.")
-    parser.add_argument("--end", type=int, default=-1, help="End index.")
-    parser.add_argument("--filter-difficulty", action="store_true", help="Filter difficulty.")
-    parser.add_argument("--result-dir", type=str, default="./", help="Result dir to save files.")
-    parser.add_argument("--check", action="store_true", help="Perform evaluation checks on generated samples.")
-    parser.add_argument("--inference", action="store_true", help="Perform inference.")
-    parser.add_argument("--temperatures", type=float, nargs="+", default=[0], help="Temperature for sampling.")
-    parser.add_argument("--math-difficulty-lower-bound", type=int, default=None, help="Lowest difficulty level for math.")
-    parser.add_argument("--math-difficulty-upper-bound", type=int, default=None, help="Highest difficulty level for math.")
-    parser.add_argument("--n", type=int, default=1, help="Number of samples generated per problem.")
-    args = parser.parse_args()
-    
+def main(args):
+
     handler: TaskHandler = TASK_HANDLERS[args.dataset]()
     temperatures = [1] if args.model.startswith("openai/o1") else args.temperatures 
     
@@ -368,8 +350,27 @@ def ray_main():
     main()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Unified inference and checking for different datasets/tasks.")
+    parser.add_argument("--dataset", type=str, required=True, choices=list(TASK_HANDLERS.keys()), help="Dataset to process.")
+    parser.add_argument("--model", type=str, required=True, default="Qwen/QwQ-32B-Preview", help="The model to run.")
+    parser.add_argument("--tp", type=int, default=8, help="Tensor Parallelism Degree")
+    parser.add_argument("--max_tokens", type=int, default=32768, help="Max tokens for the model.")
+    parser.add_argument("--split", type=str, default="train", help="Split to use for apps (e.g., train, test).")
+    parser.add_argument("--source", type=str, help="Source for the dataset.")
+    parser.add_argument("--start", type=int, default=0, help="Start index.")
+    parser.add_argument("--end", type=int, default=-1, help="End index.")
+    parser.add_argument("--filter-difficulty", action="store_true", help="Filter difficulty.")
+    parser.add_argument("--result-dir", type=str, default="./", help="Result dir to save files.")
+    parser.add_argument("--check", action="store_true", help="Perform evaluation checks on generated samples.")
+    parser.add_argument("--inference", action="store_true", help="Perform inference.")
+    parser.add_argument("--temperatures", type=float, nargs="+", default=[0], help="Temperature for sampling.")
+    parser.add_argument("--math-difficulty-lower-bound", type=int, default=None, help="Lowest difficulty level for math.")
+    parser.add_argument("--math-difficulty-upper-bound", type=int, default=None, help="Highest difficulty level for math.")
+    parser.add_argument("--n", type=int, default=1, help="Number of samples generated per problem.")
+    args = parser.parse_args()
+    
     ray.init()
 
-    ray.get(ray_main.options(resources={"GPU": 8, "anyscale/accelerator_shape:8xA10G": 0.001}).remote())
+    ray.get(ray_main.options(num_gpus=args.tp, resources={"anyscale/accelerator_shape:8xL40S": 0.001}).remote())
 
 
